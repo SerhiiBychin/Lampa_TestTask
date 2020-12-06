33edec9e4549793d797bdd195e41622799e46c50
sbychin <sbychin@plexteq.com>
2020-12-06 15:40:12 +0200

    Added MovieTableViewCell.

---
+++ Lampa_TestTask/Scenes/Movies/TableViewCell/MovieTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewCell.swift
+//  Lampa_TestTask
+//
+//  Created by Serhii Bychin on 06.12.2020.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    //MARK: - IBOutlets -
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieOverviewLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
